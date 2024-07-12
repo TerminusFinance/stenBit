b97cf36eb711eb644f42ec841751666b5ee77562
Roma29734 <95440073+Roma29734@users.noreply.github.com>
2024-07-12 22:09:01 +0300

    с

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,39 @@
-import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import React, { useEffect, useRef } from 'react';
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { SDKProvider } from "@tma.js/sdk-react";
 
 const manifestUrl = 'https://gist.githubusercontent.com/siandreev/75f1a2ccf2f3b4e2771f6089aeb06d7f/raw/d4986344010ec7a2d1cc8a2a9baa57de37aaccb8/gistfile1.txt';
 
 const AppContainer: React.FC = () => {
-    const [appHeight, setAppHeight] = useState(window.innerHeight);
+    const appRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        const updateHeight = () => setAppHeight(window.innerHeight);
+        const handleResize = () => {
+            if (appRef.current) {
+                const appHeight = appRef.current.offsetHeight;
+                document.documentElement.style.setProperty('--app-height', `${appHeight}px`);
+            }
+        };
 
-        updateHeight();  // Set initial height
-        window.addEventListener('resize', updateHeight);
+        const observer = new ResizeObserver(handleResize);
+        if (appRef.current) {
+            observer.observe(appRef.current);
+        }
+
+        handleResize();  // Set initial height
 
         return () => {
-            window.removeEventListener('resize', updateHeight);
+            if (appRef.current) {
+                observer.unobserve(appRef.current);
+            }
         };
     }, []);
 
     return (
-        <div style={{ height: appHeight }}>
+        <div ref={appRef} style={{ height: 'var(--app-height)' }}>
             <SDKProvider acceptCustomStyles debug>
                 <TonConnectUIProvider manifestUrl={manifestUrl}>
                     <React.StrictMode>
