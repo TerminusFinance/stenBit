0befdc621f281592ddc4c555138bcbe62b7fb0fe
Roma29734 <95440073+Roma29734@users.noreply.github.com>
2024-07-12 13:05:04 +0300

    update height

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import {BrowserRouter  as Router, Routes, Route, Navigate} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import About from './components/about/About';
 import NotFound from './components/notFound/NotFound';
@@ -9,16 +10,27 @@ import HomeScreen from "./components/home/Home";
 import LevelScreen from "./components/home/levelScreen/LevelScreen.tsx";
 import ProfileScreen from "./components/home/profileScreen/ProfileScreen.tsx";
 import TasksScreen from "./components/home/tasksScreen/TasksScreen.tsx";
-import React from "react";
-import {DataProvider} from "./components/DataContext.tsx";
-import {RedirectsScreen} from "./components/redirects/RedirectsScreen.tsx";
-import {FriendsScreen} from "./components/home/friendsScreen/FriendsScreen.tsx";
-import {ToastProvider} from "./components/viewComponents/toast/ToastContext.tsx";
-import {CheckScreen} from "./components/loading/CheckScreen.tsx";
-import {BoostScreen} from "./components/home/tapScreen/boostScreen/BoostScreen.tsx";
+import { DataProvider } from "./components/DataContext.tsx";
+import { RedirectsScreen } from "./components/redirects/RedirectsScreen.tsx";
+import { FriendsScreen } from "./components/home/friendsScreen/FriendsScreen.tsx";
+import { ToastProvider } from "./components/viewComponents/toast/ToastContext.tsx";
+import { CheckScreen } from "./components/loading/CheckScreen.tsx";
+import { BoostScreen } from "./components/home/tapScreen/boostScreen/BoostScreen.tsx";
 import "./App.css"
 
 const App: React.FC = () => {
+    const setAppHeight = () => {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    useEffect(() => {
+        setAppHeight();
+        window.addEventListener('resize', setAppHeight);
+        return () => {
+            window.removeEventListener('resize', setAppHeight);
+        };
+    }, []);
 
     return (
         <div className="app-container">
@@ -26,36 +38,34 @@ const App: React.FC = () => {
                 <ToastProvider>
                     <Router>
                         <Routes>
-                            <Route path="/" element={<LoadingScreen/>}/>
-                            <Route path="/loading" element={<LoadingScreen/>}/>
-                            <Route path="/check" element={<CheckScreen/>}/>
-                            <Route path="/boost" element={<BoostScreen/>}/>
-                            <Route path="/start" element={<StartScreen/>}/>
-                            <Route path="/tap" element={<TapScreen/>}/>
-                            <Route path="/friends" element={<FriendsScreen/>}/>
-                            <Route path="/tasks" element={<TasksScreen/>}/>
-                            <Route path="/profile" element={<ProfileScreen/>}/>
-                            <Route path="/home" element={<HomeScreen/>}>
-                                <Route index element={<Navigate to="tap"/>}/>
-                                <Route path="tap" element={<TapScreen/>}/>
-                                <Route path="friends" element={<FriendsScreen/>}/>
-                                <Route path="tasks" element={<TasksScreen/>}/>
-                                <Route path="profile" element={<ProfileScreen/>}/>
+                            <Route path="/" element={<LoadingScreen />} />
+                            <Route path="/loading" element={<LoadingScreen />} />
+                            <Route path="/check" element={<CheckScreen />} />
+                            <Route path="/boost" element={<BoostScreen />} />
+                            <Route path="/start" element={<StartScreen />} />
+                            <Route path="/tap" element={<TapScreen />} />
+                            <Route path="/friends" element={<FriendsScreen />} />
+                            <Route path="/tasks" element={<TasksScreen />} />
+                            <Route path="/profile" element={<ProfileScreen />} />
+                            <Route path="/home" element={<HomeScreen />}>
+                                <Route index element={<Navigate to="tap" />} />
+                                <Route path="tap" element={<TapScreen />} />
+                                <Route path="friends" element={<FriendsScreen />} />
+                                <Route path="tasks" element={<TasksScreen />} />
+                                <Route path="profile" element={<ProfileScreen />} />
                             </Route>
-                            <Route path="/level" element={<LevelScreen/>}/>
-                            <Route path="/tap" element={<TapScreen/>}/>
-                            <Route path="/about" element={<About/>}/>
-                            <Route path="/not-found" element={<NotFound/>}/>
-                            <Route path="*" element={<NotFound/>}/>
-                            <Route path="/redirects" element={<RedirectsScreen/>}/>
+                            <Route path="/level" element={<LevelScreen />} />
+                            <Route path="/tap" element={<TapScreen />} />
+                            <Route path="/about" element={<About />} />
+                            <Route path="/not-found" element={<NotFound />} />
+                            <Route path="*" element={<NotFound />} />
+                            <Route path="/redirects" element={<RedirectsScreen />} />
                         </Routes>
                     </Router>
-</ToastProvider>
+                </ToastProvider>
             </DataProvider>
         </div>
     );
 };
 
-
-
-export default App
+export default App;
