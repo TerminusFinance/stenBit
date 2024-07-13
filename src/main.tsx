ad14691b0c2c58492e5457be473b11d950660717
Roma29734 <95440073+Roma29734@users.noreply.github.com>
2024-07-13 20:56:27 +0300

    c

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { SDKProvider } from "@tma.js/sdk-react";
 import {postEvent} from "@tma.js/sdk";
@@ -10,13 +10,48 @@ const manifestUrl = 'https://gist.githubusercontent.com/siandreev/75f1a2ccf2f3b4
 
 const AppContainer: React.FC = () => {
 
+
+
     try {
         postEvent('web_app_expand');
     }catch (e) {
 
     }
+    const [height, setHeight] = useState(window.innerHeight);
+    const appRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setHeight(window.innerHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    useEffect(() => {
+        const observer = new ResizeObserver(() => {
+            if (appRef.current) {
+                setHeight(appRef.current.offsetHeight);
+            }
+        });
+
+        if (appRef.current) {
+            observer.observe(appRef.current);
+        }
+
+        return () => {
+            if (appRef.current) {
+                observer.unobserve(appRef.current);
+            }
+        };
+    }, []);
 
     return (
+        <div ref={appRef} style={{ position: 'absolute', top: 0, left: 0, right: 0, height: `${height}px` }}>
             <SDKProvider acceptCustomStyles debug>
                 <TonConnectUIProvider manifestUrl={manifestUrl}>
                     <React.StrictMode>
@@ -24,6 +59,7 @@ const AppContainer: React.FC = () => {
                     </React.StrictMode>
                 </TonConnectUIProvider>
             </SDKProvider>
+        </div>
     );
 };
 
