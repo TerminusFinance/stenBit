398db63e61e9b22e6e317dc00b32af3ef6b8df90
Roma29734 <95440073+Roma29734@users.noreply.github.com>
2024-07-13 21:09:38 +0300

    d

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { } from 'react';
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { SDKProvider } from "@tma.js/sdk-react";
 import {postEvent} from "@tma.js/sdk";
@@ -17,41 +17,9 @@ const AppContainer: React.FC = () => {
     }catch (e) {
 
     }
-    const [height, setHeight] = useState(window.innerHeight);
-    const appRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setHeight(window.innerHeight);
-        };
-
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    useEffect(() => {
-        const observer = new ResizeObserver(() => {
-            if (appRef.current) {
-                setHeight(appRef.current.offsetHeight);
-            }
-        });
-
-        if (appRef.current) {
-            observer.observe(appRef.current);
-        }
-
-        return () => {
-            if (appRef.current) {
-                observer.unobserve(appRef.current);
-            }
-        };
-    }, []);
 
     return (
-        <div ref={appRef} style={{ position: 'absolute', top: 0, left: 0, right: 0, height: `${height}px` }}>
+        <div style={{  height: `100vh` }}>
             <SDKProvider acceptCustomStyles debug>
                 <TonConnectUIProvider manifestUrl={manifestUrl}>
                     <React.StrictMode>
