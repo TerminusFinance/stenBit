7e6640a1d4249d8cc8646232016633d264b07aae
Roma29734 <95440073+Roma29734@users.noreply.github.com>
2024-07-14 23:18:42 +0300

    m

--- src/components/home/tapScreen/boostScreen/boostItem/BoostItem.tsx
+++ src/components/home/tapScreen/boostScreen/boostItem/BoostItem.tsx
@@ -15,19 +15,26 @@ interface BoostItemParams {
 export const BoostItem: React.FC<BoostItemParams> = ({ checkIcon, name, price, lvl, onClick, clickable }) => {
     return (
         <div className={`boost-item-outer-container`}>
-            <div className={`boost-item-container-${clickable}`} onClick={onClick}>
+            <div className={`boost-item-container-${clickable}`} onClick={() => clickable == true ? onClick() : {}}>
                 <img src={checkIcon} className='ic-logo' />
                 <div className="progress-text">
                     <div className="boost-tx-container">
                         <span className="current-name-boost">{name}</span>
-                        <div className='coins-container-boost-item'>
-                            <img src={CoinsIco} className='ic-coins' />
-                            <span className="divider-task-boost">{price}</span>
-                            <span className="driver-lvl-boost">{lvl}lvl</span>
-                        </div>
+                        {clickable == true ? (
+                            <div className='coins-container-boost-item'>
+                                <img src={CoinsIco} className='ic-coins'/>
+                                <span className="divider-task-boost">{price}</span>
+                                <span className="driver-lvl-boost">{lvl}lvl</span>
+                            </div>
+                        ) : (
+                            <div className='coins-container-boost-item'>
+                                <span className="driver-lvl-boost">Coming soon</span>
+                            </div>
+                        )
+                        }
                     </div>
                 </div>
-                <img src={icRightArrow} className="action-status-acc" />
+                <img src={icRightArrow} className="action-status-acc"/>
             </div>
         </div>
     );
